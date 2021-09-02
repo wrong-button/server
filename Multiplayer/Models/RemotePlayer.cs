808bf5185b162ec782e4b321f7b01c71506fcf2e
kiootic <kiootic@gmail.com>
2021-09-02 23:17:01 +0800

    Send spectator flag to client

--- Multiplayer/Models/RemotePlayer.cs
+++ Multiplayer/Models/RemotePlayer.cs
@@ -5,12 +5,14 @@
         public string Id { get; init; }
         public string Name { get; init; }
         public int Color { get; init; }
+        public bool IsSpectator { get; init; }
 
         public RemotePlayer(Player player)
         {
             this.Id = player.ConnectionId;
             this.Name = player.Data.DisplayName;
             this.Color = player.Data.PrimaryColor;
+            this.IsSpectator = player.IsSpectator;
         }
     }
 }
